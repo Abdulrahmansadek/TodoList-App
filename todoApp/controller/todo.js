88605dbddc7bdef783662a28d0049910e372c02b
Abdulrahmansadek <boody.sad@gmail.com>
2022-01-11 20:45:08 +0100

    fix

--- todoApp/controller/todo.js
+++ todoApp/controller/todo.js
@@ -7,23 +7,25 @@ const getAllTask = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  const newItem = {
-    task: req.body.task,
-    description: req.body.description,
-    id: uuidv4(),
-    time: moment().format("MMMM Do YYYY, h:mm a"),
-  };
+  const newItem = req.body;
+
   const { task, description } = newItem;
   if (!task) {
     res.status(400).render("error", {
       msg: "You did not include a task , please add task field ! ",
     });
   } else if (!description) {
-    res.status(400).render("error", {
-      msg: "You did not include a description , please add description field ! ",
-    });
+    res
+      .status(400)
+      .send(
+        "You did not include a description , please add description field ! "
+      );
   } else {
-    todoItems.push(newItem);
+    todoItems.push({
+      ...newItem,
+      time: moment().format("MMMM Do YYYY, h:mm a"),
+      id: uuidv4(),
+    });
     res.redirect("/todo");
   }
 };
@@ -39,10 +41,9 @@ const getTask = (req, res) => {
 };
 
 const updateTask = (req, res) => {
-  const { id } = req.params;
   const newTask = req.body;
   const { task, description } = newTask;
-  const foundTask = todoItems.find((todo) => todo.id === id);
+  const foundTask = todoItems.find((todo) => todo.id === req.params.id);
   if (foundTask) {
     if (task) {
       foundTask.task = task;
@@ -52,6 +53,8 @@ const updateTask = (req, res) => {
     }
 
     res.redirect("/todo");
+  } else {
+    res.render("error", { msg: "this id is not exist" });
   }
 };
 
@@ -61,8 +64,6 @@ const deleteTask = (req, res) => {
   if (foundTask) {
     todoItems = todoItems.filter((todo) => todo.id !== id);
     res.redirect("/todo");
-  } else {
-    res.status(400).send("cant find this id");
   }
 };
 
